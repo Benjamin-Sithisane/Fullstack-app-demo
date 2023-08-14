0631d13b31100d7d49110dbf84fc74b60ce881cb
Ben Sithisane <bsithisane@gmail.com>
2023-08-14 16:01:47 -0500

    added some functionality to userController and userRoutes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,10 +4,41 @@ import asyncHandler from 'express-async-handler'
 // route POST /api/users/auth
 // @access Public
 const authUser = asyncHandler(async (req, res) => {
-    res.status(401);
-    throw new Error('Something went wrong')
-
     res.status(200).json({ message: 'Auth User'})
 })
 
-export { authUser }
+//@desc Register a new user/set token
+// route POST /api/users/users
+// @access Public
+const registerUser = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: 'User registered'})
+})
+
+//@desc Logout User/set token
+// route POST /api/users/logout
+// @access Public
+const logoutUser = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: 'User logged out'})
+})
+
+//@desc Get User profile/set token
+// route GET /api/users/logout
+// @access Private
+const getUserProfile = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: 'User registered'})
+})
+
+//@desc Update User profile/set token
+// route GET /api/users/logout
+// @access Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: 'User updated'})
+})
+
+export { 
+        authUser,
+        registerUser, 
+        logoutUser,
+        getUserProfile,
+        updateUserProfile
+    }

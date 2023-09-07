7d1dbdb0078eef7f6d9571fa5d2868f24a77e0df
Ben Sithisane <bsithisane@gmail.com>
2023-09-07 10:44:47 -0500

    added products to page

--- frontend/src/components/Products.jsx
+++ frontend/src/components/Products.jsx
@@ -1,31 +1,44 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
+import { Container, Card } from "react-bootstrap";
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 function Products() {
-    const[product, setProducts] = useState([])
-    const { current: displayProduct } = useRef(['image', 'title', 'price'])
+    const [products, setProducts] = useState([]);
 
-    //TODO: need to stop infinite loop
     useEffect(() => {
-        products()
-    }, [displayProduct])
+        const fetchProducts = async () => {
+            try {
+                const res = await fetch('https://fakestoreapi.com/products/');
+                if (res.ok) {
+                    const data = await res.json();
+                    setProducts(data);
+                } else {
+                    console.error('Failed to fetch products');
+                }
+            } catch (error) {
+                console.error('Error while fetching products:', error);
+            }
+        };
+        fetchProducts();
+    }, []); // Empty dependency array to run the effect only once
 
-    const products = async () => {
-        const res = await fetch('https://fakestoreapi.com/products/1')
-
-        setProducts(await res.json())
-    } 
-
-    console.log(products())
-
-  return (
-    <div>
-        <img src={product.image} alt="" />
-        <div className="product-info">
-            <h2>"${product.title}"</h2>
-            <h3>${product.price}</h3>
+    return (
+        <div>
+            <Container>
+                <Row>
+                {products.map((product) => (
+                    <Container key={product.id} style={ { width: '10rem' }}>
+                        <Card key={product.id}>
+                            <Card.Img src={product.image} width={150}/>
+                            <Card.Subtitle>${product.price}</Card.Subtitle>
+                        </Card>
+                    </Container>
+                ))}
+                </Row>
+            </Container>
         </div>
-    </div>
-  )
+    );
 }
 
-export default Products
+export default Products;

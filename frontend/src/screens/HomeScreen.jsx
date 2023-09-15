e2c2f6ba051a5e4520f905c42b7b28bbc5e27148
Ben Sithisane <bsithisane@gmail.com>
2023-09-15 15:42:40 -0500

    removed products pages

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,11 +1,9 @@
 import Hero from "../components/Hero"
-import Products from "../components/Products"
 
 const HomeScreen = () => {
   return (
     <div>
         <Hero />
-        <Products />
     </div>
   )
 
